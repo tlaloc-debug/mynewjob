a4d75e0efcf80ad5aafc4219e7f071f68a47c987
tlaloc_debug <pelondinho@hotmail.com>
2021-10-31 14:36:15 -0400

    add menu for mobile devices and button to chat facebook

---
+++ static/admin/js/main.js
@@ -0,0 +1,15 @@
+(function ($)
+  { "use strict"
+  
+/* 1. slick Nav */
+// mobile_menu
+    var menu = $('ul#navigation');
+    if(menu.length){
+      menu.slicknav({
+        prependTo: ".mobile_menu",
+        closedSymbol: '+',
+        openedSymbol:'-'
+      });
+    };
+
+})(jQuery);
