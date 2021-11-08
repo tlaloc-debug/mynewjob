90515d14d5bd7c5668a41f822a04fa00d6e2dfb0
tlaloc_debug <pelondinho@hotmail.com>
2021-11-07 21:16:46 -0500

    update urls.py

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -7,5 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     url(r'^(?P<slug>[\w-]+)/$', views.details, name='details'),
-    path('actionUrl', views.yourSend),
+    path(r'^(?P<slug>[\w-]+)/actionUrl', views.yourSend),
 ]
